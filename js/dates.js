31513604c509f84906519bf36b3c0f3ff0e5b775
joshowen04 <owe21015@byui.edu>
2022-01-08 00:59:26 -0300

    corrected formatting

--- js/dates.js
+++ js/dates.js
@@ -1,8 +1,8 @@
-const lastmod = document.querySelector('#lastmod');
-const copywriteyear = document.querySelector('#copywriteyear');
+const lastmod = document.querySelector("#lastmod");
+const copywriteyear = document.querySelector("#copywriteyear");
 const d = new Date();
 const year = d.getFullYear();
 
-copywriteyear.textContent += `${year} | Joshua Owen | Santiago Chile`
+copywriteyear.textContent += `${year} | Joshua Owen | Santiago Chile`;
 lastmod.textContent = `This was last modified: ${document.lastModified}`;
 /*lastmod.innerHTML = */
