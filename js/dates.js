300e1d58b22eea00333ab427f3e39e803fc6c435
joshowen04 <owe21015@byui.edu>
2022-01-08 00:45:19 -0300

    verified, description,footer, title

--- js/dates.js
+++ js/dates.js
@@ -3,6 +3,6 @@ const copywriteyear = document.querySelector('#copywriteyear');
 const d = new Date();
 const year = d.getFullYear();
 
-copywriteyear.textContent += year
+copywriteyear.textContent += `${year} | Joshua Owen | Santiago Chile`
 lastmod.textContent = `This was last modified: ${document.lastModified}`;
 /*lastmod.innerHTML = */
