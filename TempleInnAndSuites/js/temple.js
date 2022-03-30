cd0920f0b63e5b1d7a24656d84d2b80d11924bb1
joshowen04 <owe21015@byui.edu>
2022-03-29 23:27:04 -0300

    templecards

--- TempleInnAndSuites/js/temple.js
+++ TempleInnAndSuites/js/temple.js
@@ -1,17 +1,23 @@
-const templeparent = document.querySelector("#availableTemples");
+const requestURL = "https://joshowen04.github.io/wdd230/TempleInnAndSuites/data/temples.json";
+let slideIndex = 1;
+
+const templeparent = document.querySelector(".templeCards");
 let templeList = [];
 const output = (temples) => {
   //console.log(temple.templeName)
   temples.forEach((temples) => {
     let card = document.createElement("section");
-    card.classList.add("templeCard")
+    card.classList.add("templeCard");
+    card.classList.add("mySlides");
+    card.classList.add("fade");
     let templeName = document.createElement("h3");
 
     templeName.innerText = temples.templeName;
+    templeName.classList.add("text");
     card.appendChild(templeName);
 
-    let location = document.createElement("h4");
-    location.innerText = temples.location;
+//    let location = document.createElement("h4");
+//    location.innerText = temples.location;
     // let dedicated = document.createElement("h4");
     // dedicated.innerText = temples.dedicated;
     // card.appendChild(dedicated);
@@ -19,32 +25,110 @@ const output = (temples) => {
     imageUrl.src = temples.imageUrl;
     imageUrl.alt = temples.templeName;
     card.appendChild(imageUrl);
-    card.appendChild(location);
+    //card.appendChild(location);
+    let lat = temples.lat
+    let lon = temples.lon
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=46987a429fccce36bf013c9455dc421d`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        weather = data;
+  
+        console.log(weather)
+        let weatherinfo = document.createElement("div");
+        
+        /* Current Temp */
+        let temp = weather.current.temp;
+        let conditions = weather.current.weather[0].main;
+        let conDesc = weather.current.weather[0].description;
+        let humidity = weather.current.humidity;
+        let icon = `https://openweathermap.org/img/w/${weather.current.weather[0].icon}.png`;
 
-    templeparent.appendChild(card);
+        /*future dates */
+
+        let one = parseInt(`${weather.daily[1].dt}000`)
+        let oneFullDate= new Date(one);
+      
+        let oneRefinedDate = `${oneFullDate.getDate()}/${oneFullDate.getMonth()+1}/${oneFullDate.getFullYear()}`
+
+        let two = parseInt(`${weather.daily[2].dt}000`)
+        let twoFullDate= new Date(two);
+      
+        let twoRefinedDate = `${twoFullDate.getDate()}/${twoFullDate.getMonth()+1}/${twoFullDate.getFullYear()}`
+
+        let three = parseInt(`${weather.daily[3].dt}000`)
+        let threeFullDate= new Date(three);
+      
+        let threeRefinedDate = `${threeFullDate.getDate()}/${threeFullDate.getMonth()+1}/${threeFullDate.getFullYear()}`
+
+        /*future temps*/
+        let oneTemp = weather.daily[1].temp.day;
+        let twoTemp = weather.daily[2].temp.day;
+        let threeTemp = weather.daily[3].temp.day;
+
+        
+        let currentConditions = document.createElement("div");
+        currentConditions.innerText = `Current: ${conDesc}, ${temp}\u00B0 F and ${humidity}% Humidity`;
+        
+        let oneWeather = document.createElement("div");
+        oneWeather.innerHTML = `<h4>${oneRefinedDate}</h4><p>${oneTemp}\u00B0 F</p>`;
+        let twoWeather = document.createElement("div");
+        twoWeather.innerHTML = `<h4>${twoRefinedDate}</h4><p>${twoTemp}\u00B0 F</p>`;
+        let threeWeather = document.createElement("div");
+        threeWeather.innerHTML = `<h4>${threeRefinedDate}</h4><p>${threeTemp}\u00B0 F</p>`;
+
+        let forecast = document.createElement("div");
+        forecast.append(oneWeather,twoWeather,threeWeather,)
+
+
+        weatherinfo.append(currentConditions,forecast);
+        weatherinfo.classList.add("weatherinfo")
+        forecast.classList.add("weatherForecast")
+
+
+
+
+        card.appendChild(weatherinfo);
+        templeparent.appendChild(card);
+        showSlides(slideIndex);
+      });
+
   });
+
 };
 
-function addWeather(card) {
-  weatherSection = document.createElement("section");
-  weatherSection.classList.add("weatherSection");
-  weatherSection.innerText="Weather";
-  card.appendChild(weatherSection);
-  console.log(card);
-}
-// Step 3: Using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'
-fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json")
+
+fetch(requestURL)
   .then((response) => response.json())
   .then((data) => {
     templeList = data;
     output(templeList);
-    let allTemples = document.querySelectorAll(".templeCard");
-    
-    allTemples = [...allTemples]
-    
-    allTemples.map(addWeather);
-
   });
+
+
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  //console.log(`slides  -  ${slides[0]}`);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "flex";
+}
+
+
+/*
 
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
@@ -64,8 +148,6 @@ function reset() {
   }
 }
 
-
-/* -- My code */
 function sortBy() {
   reset();
   let sortOption = document.querySelector("#sortBy").value;
@@ -103,3 +185,4 @@ const filterBy = () => {
 
 document.querySelector("#sortBy").addEventListener("change", sortBy);
 document.querySelector("#filterBy").addEventListener("change", filterBy);
+*/
